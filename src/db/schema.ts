7a435174ffeb1dd159b4c0656d945d9227e001aa
re2osushi8888 <re.2osushi8888@gmail.com>
2025-03-06 11:50:59 +0000

    schemaを作る

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+
+export const userTable = sqliteTable('users_table', {
+  id: int().primaryKey({autoIncrement: true}),
+  name: text().notNull(),
+  age: int().notNull(),
+  email: text().notNull().unique()
+})
