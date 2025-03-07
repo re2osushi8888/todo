38d3b8c5e6d227cec804de43bb1efe540ed51e67
re2osushi8888 <re.2osushi8888@gmail.com>
2025-03-07 12:23:37 +0000

    dbをモックするテストを書いてみる。最初だけ

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import { createClient } from '@libsql/client';
+import { Database } from 'bun:sqlite';
+import { drizzle as drizzleBunSqlite } from 'drizzle-orm/bun-sqlite';
+import { drizzle as drizzleSqlite } from 'drizzle-orm/libsql';
+import type { BaseSQLiteDatabase } from 'drizzle-orm/sqlite-core';
 import { Hono } from 'hono';
+import type * as schema from './db/schema';
+import { usersTable } from './db/schema';
 
 const app = new Hono();
 
@@ -22,12 +29,26 @@ app.post('/posts', (c) => {
 	);
 });
 
-app.get('/user', (c) => {
+app.get('/user', async (c) => {
+	// biome-ignore lint:suspicious/noExplicitAny:
+	type SqliteDB = BaseSQLiteDatabase<any, any, typeof schema>;
+	function createDB(): SqliteDB {
+		let db: SqliteDB;
+		if (process.env.IS_TEST === 'True') {
+			const sqlite = new Database('sqlite.db');
+			db = drizzleBunSqlite(sqlite);
+		} else {
+			const client = createClient({ url: process.env.DB_FILE_NAME as string });
+			db = drizzleSqlite({ client });
+		}
+		return db;
+	}
+	const db: SqliteDB = createDB();
+	// FIXME: select文かmodelを作らないといけない
+	const users = await db.select().from(usersTable);
+	const user = users[0];
 	return c.json({
-		id: 1,
-		name: 'Allen',
-		age: 27,
-		email: 'allen@example.com',
+		user,
 	});
 });
 
--- tests/dbMock.test.ts
+++ tests/dbMock.test.ts
@@ -1,14 +1,38 @@
-import { expect, test } from 'bun:test';
+import { Database } from 'bun:sqlite';
+import { afterAll, beforeAll, expect, test } from 'bun:test';
+import { drizzle as drizzleBunSqlite } from 'drizzle-orm/bun-sqlite';
+import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 import app from '../src';
+import { usersTable } from '../src/db/schema';
 
-test('', async () => {
-	const res = await app.request('/user');
+const sqlite = new Database('sqlite.db');
+const db = drizzleBunSqlite(sqlite);
 
-	expect(res.status).toBe(200);
-	expect(await res.json()).toEqual({
+beforeAll(async () => {
+	process.env.IS_TEST = 'True';
+	migrate(db, { migrationsFolder: '/workspace/drizzle' });
+
+	const user: typeof usersTable.$inferInsert = {
 		id: 1,
 		name: 'Allen',
 		age: 27,
 		email: 'allen@example.com',
+	};
+	await db.insert(usersTable).values(user);
+});
+afterAll(async () => {
+	await db.delete(usersTable);
+});
+test('GET /user', async () => {
+	const res = await app.request('/user');
+
+	expect(res.status).toBe(200);
+	expect(await res.json()).toEqual({
+		user: {
+			id: 1,
+			name: 'Allen',
+			age: 27,
+			email: 'allen@example.com',
+		},
 	});
 });
