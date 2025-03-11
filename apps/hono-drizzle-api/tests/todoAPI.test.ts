76747097ead0b5adfbd736108f9fd36d46e648a0
re2osushi8888 <re.2osushi8888@gmail.com>
2025-03-11 12:17:29 +0000

    testでDBを用意する

--- apps/hono-drizzle-api/tests/todoAPI.test.ts
+++ apps/hono-drizzle-api/tests/todoAPI.test.ts
@@ -1,6 +1,19 @@
-import { describe, test } from 'bun:test';
+import { afterAll, beforeAll, describe, test } from 'bun:test';
+import { createTestDB } from '../src/db/createDb';
+import { todoItemsTable } from '../src/db/schema';
 
-describe('todoAPI', () => {
+describe('todoAPI', async () => {
+  const db = createTestDB()
+  beforeAll(async () => {
+    const cleanTodo: typeof todoItemsTable.$inferInsert = {
+      title: '掃除する',
+    };
+    await db.insert(todoItemsTable).values(cleanTodo)
+  })
+  afterAll(async () => {
+    await db.delete(todoItemsTable)
+  })
+
   describe('GET /todo', () => {
     test.todo('全てのtodoが取得できる')
   })
