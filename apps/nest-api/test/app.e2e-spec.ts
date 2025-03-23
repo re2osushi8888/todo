48343bdc4530ff59a2f6fbdeee94136d73a448e8
re2osushi8888 <re.2osushi8888@gmail.com>
2025-03-23 06:25:30 +0000

    esmoduleにする

--- apps/nest-api/test/app.e2e-spec.ts
+++ apps/nest-api/test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import supertest from 'supertest';
-import { App } from 'supertest/types';
+import { App } from 'supertest/types.js';
 import { beforeEach, describe, it } from 'vitest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from './../src/app.module.js';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
