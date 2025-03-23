8a7f740848a0beb0cafc0cee70d7afe4989c15b8
re2osushi8888 <re.2osushi8888@gmail.com>
2025-03-23 06:06:35 +0000

    e2eテスト修正

--- apps/nest-api/test/app.e2e-spec.ts
+++ apps/nest-api/test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing';
+import supertest from 'supertest';
 import { App } from 'supertest/types';
+import { beforeEach, describe, it } from 'vitest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -17,7 +18,7 @@ describe('AppController (e2e)', () => {
   });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return supertest(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
