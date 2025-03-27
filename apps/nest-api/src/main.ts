6c359792f9ec2cc66d318ef35161bc92dacc921a
re2osushi8888 <re.2osushi8888@gmail.com>
2025-03-27 13:07:41 +0000

    ValidationPipeを使う

--- apps/nest-api/src/main.ts
+++ apps/nest-api/src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(env.PORT);
 }
 bootstrap();
