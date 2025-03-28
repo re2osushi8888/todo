2617924fba9968318f7c266d25247b21fa9310e8
re2osushi8888 <re.2osushi8888@gmail.com>
2025-03-28 09:00:24 +0000

    Prismaモジュールを作成

--- apps/nest-api/src/app.module.ts
+++ apps/nest-api/src/app.module.ts
@@ -6,9 +6,10 @@ import { ItemsController } from './items/items.controller';
 import { ItemsModule } from './items/items.module';
 import { ItemsService } from './items/items.service';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, ItemsModule],
+  imports: [ConfigModule.forRoot(), UsersModule, ItemsModule, PrismaModule],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
