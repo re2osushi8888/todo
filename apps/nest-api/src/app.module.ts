51ea712b09f5b2e17f21cc5b7ac4ef2e623769e7
re2osushi8888 <re.2osushi8888@gmail.com>
2025-03-29 08:24:02 +0000

    authModule作成

--- apps/nest-api/src/app.module.ts
+++ apps/nest-api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { ItemsController } from './items/items.controller';
 import { ItemsModule } from './items/items.module';
 import { ItemsService } from './items/items.service';
@@ -9,7 +10,13 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, ItemsModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    ItemsModule,
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
