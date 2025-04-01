eb1a13316b7c9c5484da290ab6bff50dc024d2db
re2osushi8888 <re.2osushi8888@gmail.com>
2025-04-01 12:49:05 +0000

    jwt認証ができるようにする

--- apps/nest-api/src/auth/auth.module.ts
+++ apps/nest-api/src/auth/auth.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
