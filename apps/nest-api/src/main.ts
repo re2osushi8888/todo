d78b4356d4960f1d52f10547f876f6f0c9c55922
re2osushi8888 <re.2osushi8888@gmail.com>
2025-04-02 13:34:22 +0000

    CORSの設定

--- apps/nest-api/src/main.ts
+++ apps/nest-api/src/main.ts
@@ -5,6 +5,14 @@ import { env } from './env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全てのドメインから許可するCORSの設定
+  app.enableCors()
+
+  // 一部のドメインからGETとPOSTだけ許可するCORSの設定
+  // app.enableCors({
+  //   origin: ['http://example.com:1234'],
+  //   methods: ['GET', 'POST']
+  // });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(env.PORT);
 }
